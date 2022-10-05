1ed93643a401ea651e151c1b1fb259e6db863a8b
Abner Suhett <suhettabner@gmail.com>
2022-10-04 22:46:03 -0300

    Passando o Projeto antigo para React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import Home from "./components/pages/Home";
+import AdminPainel from "./components/pages/AdminPainel";
+
+import './styles/style.css'
+import './styles/AdminPainel.css'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/user/:userid/painel" element={<AdminPainel />} />
+    </Routes>
+    </BrowserRouter>
+    
   );
 }
 
